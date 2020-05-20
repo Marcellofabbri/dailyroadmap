33a064be91dc13be8361a73d57b8aa9dcca5d517
Marcello Fabbri <marcellogfabbri@gmail.com>
2020-05-20 21:49:23 +0100

    add EventDao (Data Access Object) for CRUD operations with the database

---
+++ app/src/main/java/eu/marcellofabbri/dailyroadmap/EventDao.java
@@ -0,0 +1,28 @@
+package eu.marcellofabbri.dailyroadmap;
+
+import androidx.lifecycle.LiveData;
+import androidx.room.Dao;
+import androidx.room.Delete;
+import androidx.room.Insert;
+import androidx.room.Query;
+import androidx.room.Update;
+
+import java.util.List;
+
+@Dao
+public interface EventDao {
+    @Insert
+    void insert(Event event);
+
+    @Update
+    void update(Event event);
+
+    @Delete
+    void delete(Event event);
+
+    @Query("DELETE FROM Event_table")
+    void deleteAllEvents();
+
+    @Query("SELECT * FROM Event_table ORDER BY startTime DESC")
+    LiveData<List<Event>> getAllEvents();
+}

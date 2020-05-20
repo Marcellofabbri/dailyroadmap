a34883d7300c270abce76be9aabf2690afcadd06
Marcello Fabbri <marcellogfabbri@gmail.com>
2020-05-21 00:13:58 +0100

    changes to EventDatabase to execute a Callback that inserts default initial events upon instantiation of the DB

--- app/src/main/java/eu/marcellofabbri/dailyroadmap/EventDatabase.java
+++ app/src/main/java/eu/marcellofabbri/dailyroadmap/EventDatabase.java
@@ -1,10 +1,19 @@
 package eu.marcellofabbri.dailyroadmap;
 
 import android.content.Context;
+import android.os.AsyncTask;
+import android.os.Build;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.RequiresApi;
+import androidx.room.Dao;
 import androidx.room.Database;
 import androidx.room.Room;
 import androidx.room.RoomDatabase;
+import androidx.sqlite.db.SupportSQLiteDatabase;
+
+import java.time.LocalDateTime;
+import java.time.Month;
 
 @Database(entities = Event.class, version = 1)
 public abstract class EventDatabase extends RoomDatabase {
@@ -17,9 +26,34 @@ public abstract class EventDatabase extends RoomDatabase {
     public static synchronized EventDatabase getInstance(Context context) {
         if (instance == null) {
             instance = Room.databaseBuilder(context.getApplicationContext(), EventDatabase.class, "Event_database")
+                    .addCallback(roomCallback) //to call the onCreate method
                     .fallbackToDestructiveMigration()
                     .build();
         }
         return instance;
+    }
+
+    private static RoomDatabase.Callback roomCallback = new RoomDatabase.Callback() {
+        @Override
+        public void onCreate(@NonNull SupportSQLiteDatabase db) {
+            super.onCreate(db);
+            new PopulateDbAsyncTask(instance).execute();
+        }
+    };
+
+    private static class PopulateDbAsyncTask extends AsyncTask<Void, Void, Void> {
+        private EventDao eventDao;
+
+        private PopulateDbAsyncTask(EventDatabase eventDatabase) {
+            eventDao = eventDatabase.eventDao();
+        }
+
+        @RequiresApi(api = Build.VERSION_CODES.O)
+        @Override
+        protected Void doInBackground(Void...voids) {
+            eventDao.insert(new Event("event description", LocalDateTime.of(2015, Month.JANUARY, 01, 8, 00, 00), LocalDateTime.of(2015, Month.JANUARY, 01, 9, 00, 00)));
+            eventDao.insert(new Event("event description", LocalDateTime.of(2015, Month.JANUARY, 01, 10, 00, 00), LocalDateTime.of(2015, Month.JANUARY, 01, 11, 00, 00)));
+            return null;
+        }
     }
 }

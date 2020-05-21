fb4b556a044bd2bac982d5dc9c59f00cd854ccd0
Marcello Fabbri <marcellogfabbri@gmail.com>
2020-05-21 11:51:50 +0100

    add a ViewModel reference to MainActivity

--- app/src/main/java/eu/marcellofabbri/dailyroadmap/MainActivity.java
+++ app/src/main/java/eu/marcellofabbri/dailyroadmap/MainActivity.java
@@ -1,14 +1,30 @@
 package eu.marcellofabbri.dailyroadmap;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.lifecycle.Observer;
+import androidx.lifecycle.ViewModelProvider;
+import androidx.lifecycle.ViewModelProviders;
 
 import android.os.Bundle;
 
+import java.util.List;
+
 public class MainActivity extends AppCompatActivity {
+
+    private EventViewModel eventViewModel;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        eventViewModel = ViewModelProviders.of(this).get(EventViewModel.class);
+        eventViewModel.getAllEvents().observe(this, new Observer<List<Event>>() {
+
+            @Override
+            public void onChanged(List<Event> events) {
+                //RecyclerView
+            }
+        });
     }
 }

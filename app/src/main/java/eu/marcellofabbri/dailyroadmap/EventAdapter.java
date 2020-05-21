c4365786376c9c195d0a9b15d347c5e35ed58a5e
Marcello Fabbri <marcellogfabbri@gmail.com>
2020-05-21 23:48:13 +0100

    add EventAdapter

--- app/src/main/java/eu/marcellofabbri/dailyroadmap/EventAdapter.java
+++ app/src/main/java/eu/marcellofabbri/dailyroadmap/EventAdapter.java
@@ -1,20 +1,54 @@
 package eu.marcellofabbri.dailyroadmap;
 
+import android.view.LayoutInflater;
 import android.view.View;
+import android.view.ViewGroup;
 import android.widget.TextView;
 
 import androidx.annotation.NonNull;
 import androidx.recyclerview.widget.RecyclerView;
 
-public class EventAdapter extends RecyclerView.Adapter {
+import java.util.ArrayList;
+import java.util.List;
+
+public class EventAdapter extends RecyclerView.Adapter<EventAdapter.EventHolder> {
+    private List<Event> events = new ArrayList<>();
+
+    @NonNull
+    @Override
+    public EventHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View itemView = LayoutInflater.from(parent.getContext()).inflate(R.layout.event_item, parent, false);
+        return new EventHolder(itemView);
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull EventHolder holder, int position) {
+        Event currentEvent = events.get(position);
+        holder.textViewDescription.setText(currentEvent.getDescription());
+        holder.textViewStartTime.setText(currentEvent.getStartTime().toString());
+        holder.textViewFinishTime.setText(currentEvent.getFinishTime().toString());
+    }
+
+    @Override
+    public int getItemCount() {
+        return events.size();
+    }
+
+    public void setEvents(List<Event> events) {
+        this.events = events;
+        notifyDataSetChanged();
+    }
 
     class EventHolder extends RecyclerView.ViewHolder{
-        private TextView TextViewDescription;
-        private TextView TextViewStartTime;
-        private TextView TextViewFinishTime;
+        private TextView textViewDescription;
+        private TextView textViewStartTime;
+        private TextView textViewFinishTime;
 
         public EventHolder(@NonNull View itemView) {
             super(itemView);
+            textViewDescription = itemView.findViewById(R.id.text_view_description);
+            textViewStartTime = itemView.findViewById(R.id.text_view_start_time);
+            textViewFinishTime = itemView.findViewById(R.id.text_view_finish_time);
         }
     }
 }
